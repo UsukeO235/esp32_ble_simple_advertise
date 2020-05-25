e044f0339d934bb98f523072fd9b27828be7a7db
UsukeO235 <usukeo235@gmail.com>
2020-05-26 00:39:51 +0900

    Fix NVS not initialized error

--- main/main.cpp
+++ main/main.cpp
@@ -76,6 +76,16 @@ extern "C"
 {
 void app_main(void)
 {
+	esp_err_t ret = nvs_flash_init();
+	if( ret == ESP_ERR_NVS_NO_FREE_PAGES )
+	{
+		ESP_ERROR_CHECK( nvs_flash_erase() );
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK( ret );
+
+
+
 	ESP_ERROR_CHECK( esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT) );
 
 	esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
